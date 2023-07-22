3a12c8d9f6698b8cf76aa21790d9d8cec6eab644
Hendrik <git@h3ndrk.de>
2023-07-22 22:18:48 +0200

    Implement nearly all of the API

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@ mod bindings;
 
 use std::{ffi::CString, path::Path, ptr::null_mut};
 
-use bindings::{op_free, op_open_file, op_pcm_total, op_read_float};
+use bindings::{
+    op_bitrate, op_bitrate_instant, op_channel_count, op_current_link, op_free, op_head,
+    op_link_count, op_open_file, op_open_memory, op_open_url, op_pcm_seek, op_pcm_tell,
+    op_pcm_total, op_raw_seek, op_raw_tell, op_raw_total, op_read, op_read_float,
+    op_read_float_stereo, op_read_stereo, op_seekable, op_serialno, op_tags, op_test_file,
+    op_test_memory, op_test_open, op_test_url,
+};
 use num_derive::{FromPrimitive, ToPrimitive};
 use num_traits::FromPrimitive;
 
@@ -51,7 +57,7 @@ pub enum OpusFileError {
 pub struct OggOpusFile(*mut bindings::OggOpusFile);
 
 impl OggOpusFile {
-    pub fn open_file(path: impl AsRef<Path>) -> Result<OggOpusFile, OpusFileError> {
+    pub fn open_file(path: impl AsRef<Path>) -> Result<Self, OpusFileError> {
         let path = CString::new(path.as_ref().to_str().unwrap()).expect("CString::new() failed");
         let mut error = 0;
         let handle = unsafe { op_open_file(path.as_ptr(), &mut error) };
@@ -62,10 +68,183 @@ impl OggOpusFile {
         }
     }
 
+    pub fn open_memory(data: &[u8]) -> Result<Self, OpusFileError> {
+        let mut error = 0;
+        let handle = unsafe { op_open_memory(data.as_ptr(), data.len(), &mut error) };
+        if handle.is_null() || error < 0 {
+            Err(OpusFileError::from_i32(error).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(Self(handle))
+        }
+    }
+
+    pub fn open_url(url: impl AsRef<Path>) -> Result<Self, OpusFileError> {
+        let url = CString::new(url.as_ref().to_str().unwrap()).expect("CString::new() failed");
+        let mut error = 0;
+        let handle = unsafe { op_open_url(url.as_ptr(), &mut error) };
+        if handle.is_null() || error < 0 {
+            Err(OpusFileError::from_i32(error).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(Self(handle))
+        }
+    }
+
+    pub fn test_file(path: impl AsRef<Path>) -> Result<Self, OpusFileError> {
+        let path = CString::new(path.as_ref().to_str().unwrap()).expect("CString::new() failed");
+        let mut error = 0;
+        let handle = unsafe { op_test_file(path.as_ptr(), &mut error) };
+        if handle.is_null() || error < 0 {
+            Err(OpusFileError::from_i32(error).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(Self(handle))
+        }
+    }
+
+    pub fn test_memory(data: &[u8]) -> Result<Self, OpusFileError> {
+        let mut error = 0;
+        let handle = unsafe { op_test_memory(data.as_ptr(), data.len(), &mut error) };
+        if handle.is_null() || error < 0 {
+            Err(OpusFileError::from_i32(error).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(Self(handle))
+        }
+    }
+
+    pub fn test_url(url: impl AsRef<Path>) -> Result<Self, OpusFileError> {
+        let url = CString::new(url.as_ref().to_str().unwrap()).expect("CString::new() failed");
+        let mut error = 0;
+        let handle = unsafe { op_test_url(url.as_ptr(), &mut error) };
+        if handle.is_null() || error < 0 {
+            Err(OpusFileError::from_i32(error).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(Self(handle))
+        }
+    }
+
+    pub fn test_open(self) -> Result<Self, OpusFileError> {
+        let result = unsafe { op_test_open(self.0) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(self)
+        }
+    }
+
+    pub fn seekable(&self) -> bool {
+        unsafe { op_seekable(self.0) != 0 }
+    }
+
+    pub fn link_count(&self) -> usize {
+        unsafe { op_link_count(self.0) as usize }
+    }
+
+    pub fn serial_number_of_link(&self, link_index: i32) -> u32 {
+        unsafe { op_serialno(self.0, link_index) }
+    }
+
+    pub fn channel_count(&self, link_index: i32) -> usize {
+        unsafe { op_channel_count(self.0, link_index) as usize }
+    }
+
+    pub fn raw_total(&self, link_index: i32) -> Result<usize, OpusFileError> {
+        let result = unsafe { op_raw_total(self.0, link_index) };
+        if result < 0 {
+            Err(OpusFileError::from_i64(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result.try_into().unwrap())
+        }
+    }
+
     pub fn pcm_total(&self, link_index: i32) -> Result<usize, OpusFileError> {
         let result = unsafe { op_pcm_total(self.0, link_index) };
         if result < 0 {
             Err(OpusFileError::from_i64(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result.try_into().unwrap())
+        }
+    }
+
+    pub fn head(&self, link_index: i32) -> Result<OpusHead, OpusFileError> {
+        let result = unsafe { op_head(self.0, link_index) };
+        if result.is_null() {
+            Err(OpusFileError::Unknown)
+        } else {
+            Ok(OpusHead(result))
+        }
+    }
+
+    pub fn tags(&self, link_index: i32) -> Result<OpusTags, OpusFileError> {
+        let result = unsafe { op_tags(self.0, link_index) };
+        if result.is_null() {
+            Err(OpusFileError::Unknown)
+        } else {
+            Ok(OpusTags(result))
+        }
+    }
+
+    pub fn current_link(&self) -> Result<i32, OpusFileError> {
+        let result = unsafe { op_current_link(self.0) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result)
+        }
+    }
+
+    pub fn bitrate(&self, link_index: i32) -> Result<i32, OpusFileError> {
+        let result = unsafe { op_bitrate(self.0, link_index) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result)
+        }
+    }
+
+    pub fn bitrate_instant(&self) -> Result<i32, OpusFileError> {
+        let result = unsafe { op_bitrate_instant(self.0) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result)
+        }
+    }
+
+    pub fn raw_tell(&self) -> Result<i64, OpusFileError> {
+        let result = unsafe { op_raw_tell(self.0) };
+        if result < 0 {
+            Err(OpusFileError::from_i64(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result)
+        }
+    }
+
+    pub fn pcm_tell(&self) -> Result<i64, OpusFileError> {
+        let result = unsafe { op_pcm_tell(self.0) };
+        if result < 0 {
+            Err(OpusFileError::from_i64(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result)
+        }
+    }
+
+    pub fn read(
+        &self,
+        buffer: &mut [i16],
+        link_index: Option<&mut i32>,
+    ) -> Result<usize, OpusFileError> {
+        let link_index = link_index
+            .map(|link_index| link_index as *mut _)
+            .unwrap_or(null_mut());
+        let result = unsafe {
+            op_read(
+                self.0,
+                buffer.as_mut_ptr(),
+                buffer.len().try_into().unwrap(),
+                link_index,
+            )
+        };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
         } else {
             Ok(result.try_into().unwrap())
         }
@@ -93,10 +272,70 @@ impl OggOpusFile {
             Ok(result.try_into().unwrap())
         }
     }
+
+    pub fn read_stereo(&self, buffer: &mut [i16]) -> Result<usize, OpusFileError> {
+        let result = unsafe {
+            op_read_stereo(
+                self.0,
+                buffer.as_mut_ptr(),
+                buffer.len().try_into().unwrap(),
+            )
+        };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result.try_into().unwrap())
+        }
+    }
+
+    pub fn read_float_stereo(&self, buffer: &mut [f32]) -> Result<usize, OpusFileError> {
+        let result = unsafe {
+            op_read_float_stereo(
+                self.0,
+                buffer.as_mut_ptr(),
+                buffer.len().try_into().unwrap(),
+            )
+        };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(result.try_into().unwrap())
+        }
+    }
+
+    pub fn raw_seek(&self, byte_offset: i64) -> Result<(), OpusFileError> {
+        let result = unsafe { op_raw_seek(self.0, byte_offset) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn pcm_seek(&self, sample_offset: i64) -> Result<(), OpusFileError> {
+        let result = unsafe { op_pcm_seek(self.0, sample_offset) };
+        if result < 0 {
+            Err(OpusFileError::from_i32(result).unwrap_or(OpusFileError::Unknown))
+        } else {
+            Ok(())
+        }
+    }
 }
 
 impl Drop for OggOpusFile {
     fn drop(&mut self) {
         unsafe { op_free(self.0) }
     }
+}
+
+pub struct OpusHead(pub *const bindings::OpusHead);
+
+impl OpusHead {
+    // TODO
+}
+
+pub struct OpusTags(pub *const bindings::OpusTags);
+
+impl OpusTags {
+    // TODO
 }
